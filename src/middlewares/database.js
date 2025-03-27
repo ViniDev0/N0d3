218f47fef73b0694578f0717aec6ad1e8360789e
Sunginha <contato.vini0dev@gmail.com>
2025-03-27 10:09:45 -0300

    Database improvement

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,21 +1,41 @@
+import fs from 'node:fs/promises';
 
-
+const dataBasePath = new URL('../../db.json', import.meta.url);
+console.log(dataBasePath);
 export class DataBase {
-  database = {}
+  #database = {}
+
+  constructor() {
+    fs.readFile(dataBasePath, 'utf-8').then(data => {
+      this.#database = JSON.parse(data);
+    })
+    .catch(() => {
+      this.#persist();
+    })
+  }
+  // Escreve em arquivos as informações.
+  #persist() {
+    fs.writeFile('db.json', JSON.stringify(this.#database)); // Escreve um arquivo na root por padrão.
+  }
+
   select(table) {
-    const data = this.database[table] ?? [];
+
+    const data = this.#database[table] ?? [];
 
     return data;
+
   }
   insert(table, data) {
     // Verifica se existe um Array dentro do objeto.
-    if (Array.isArray(this.database[table])) {
-      this.database[table].push(data)
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
 
     // Caso não tenha ele insere o elemento dentro da tabela.
     } else {
-      this.database[table] = [data];
+      this.#database[table] = [data];
     }
+
+    this.#persist(); // Após toda inserção de dados é chamado.
 
     return data;
   }
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import { randomUUID } from 'node:crypto';
 import { json } from './middlewares/json.js';
 import { DataBase } from './middlewares/database.js';
 
@@ -22,7 +23,7 @@ const server = http.createServer(async (req, res) => {
 
     // Mostra o usuário na GET.
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     }
